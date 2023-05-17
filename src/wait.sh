6f3025e3973020f00177f331af6b5b6e586c5349
David Gardiner <david@gardiner.net.au>
2023-05-17 16:58:57 +0930

    Revert "Use new go-sqlcmd"
    This reverts commit d14b250d4491abd18703eed5b56c09fc97e180d3.

--- src/wait.sh
+++ src/wait.sh
@@ -36,15 +36,13 @@ if [ "$verbose" == "true" ]; then
     echo "password: $password"
 fi
 
-./sqlcmd --version
-
 while [ "$state" != "" ]; do
     count=$((count+1))
 
     # -C        trust server certificate
     # -h -1     Don't print headings
     # -W        remove trailing spaces
-    state=`./sqlcmd -C -S $server -U $username -P "$password" -Q 'SET NOCOUNT ON; SELECT name, state_desc from sys.databases WHERE state NOT IN (0, 6, 10)' -h -1 -W -s " " `
+    state=`/opt/mssql-tools18/bin/sqlcmd -C -S $server -U $username -P "$password" -Q 'SET NOCOUNT ON; SELECT name, state_desc from sys.databases WHERE state NOT IN (0, 6, 10)' -h -1 -W -s " " `
 
     if [ $? -ne 0 ]; then
         state="Error connecting"
